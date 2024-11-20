f2b822c8a7ef38063063a3f5a0a71e7afcf40693
huijuhnkim <huijuhnkim.dev@gmail.com>
2024-11-19 20:15:21 -0500

    Initial Commit

---
+++ arietta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  arietta
+//
+//  Created by Hui Juhn Kim on 11/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

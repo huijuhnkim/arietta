f7c8af4740e26073dd9240afef9496f0c2cf1a37
christinakangg <christinakang0628@gmail.com>
2024-12-08 16:14:53 -0500

    edited ear training audio buttons

---
+++ arietta/_Profile/Progress Report/ProgressReportView.swift
@@ -0,0 +1,29 @@
+//
+//  ProgressReportView.swift
+//  arietta
+//
+//  Created by Christina Kang on 12/8/24.
+//
+
+import UIKit
+
+class ProgressReportView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

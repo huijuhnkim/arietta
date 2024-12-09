8a2933245079ebb22bfa7954683105dbbaf14f4f
Fangfei-Yang <fangfeiyang1010@gmail.com>
2024-12-08 22:56:10 -0500

    add profile pic

--- arietta/_Profile/ProfileViewController.swift
+++ arietta/_Profile/ProfileViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseStorage
 
 class ProfileViewController: UIViewController {
     
@@ -33,6 +34,7 @@ class ProfileViewController: UIViewController {
                 } else {
                     self.profileView.labelUsername.text = user?.displayName
                     self.profileView.labelEmail.text = user?.email
+                    self.downloadAndDisplayProfilePicture()
                 }
             }
         }
@@ -41,6 +43,8 @@ class ProfileViewController: UIViewController {
         super.viewDidLoad()
         
         profileView.buttonSignOut.addTarget(self, action: #selector(onButtonSignOutTapped), for: .touchUpInside)
+        
+        profileView.buttonProfilePicture.addTarget(self, action: #selector(editProfilePictureTapped), for: .touchUpInside)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -62,5 +66,53 @@ class ProfileViewController: UIViewController {
         logoutAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         
         self.present(logoutAlert, animated: true)
+    }
+    
+    func uploadProfilePicture(image: UIImage) {
+        guard let imageData = image.jpegData(compressionQuality: 0.4),
+              let userId = Auth.auth().currentUser?.uid else { return }
+        let storageRef = Storage.storage().reference().child("profile_pictures/\(userId)/avatar.jpg")
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        
+        storageRef.putData(imageData, metadata: metadata) { metadata, error in
+            if let error = error {
+                print("Error uploading image: \(error)")
+            } else {
+                print("Profile picture uploaded successfully")
+                self.downloadAndDisplayProfilePicture()  // Refresh the displayed image
+            }
+        }
+    }
+    
+    func downloadAndDisplayProfilePicture() {
+        guard let userId = Auth.auth().currentUser?.uid else { return }
+        let storageRef = Storage.storage().reference().child("profile_pictures/\(userId)/avatar.jpg")
+        storageRef.getData(maxSize: 1 * 1024 * 1024) { data, error in
+            if let error = error {
+                print("Error downloading image: \(error)")
+            } else if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.profileView.buttonProfilePicture.setImage(image, for: .normal)
+                }
+            }
+        }
+    }
+    
+    @objc func editProfilePictureTapped() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+}
+
+
+extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let editedImage = info[.editedImage] as? UIImage else { return }
+        uploadProfilePicture(image: editedImage)
+        picker.dismiss(animated: true, completion: nil)
     }
 }

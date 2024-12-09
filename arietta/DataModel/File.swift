6ab9fabfd603ef769faf3271fc7689a90f49e8ab
christinakangg <christinakang0628@gmail.com>
2024-12-08 20:45:24 -0500

    saveResult function added

--- arietta/DataModel/File.swift
+++ arietta/DataModel/File.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+struct ETResult: Codable {
+    let date: Date
+    let difficulty: Int
+    let score: Int
+    
+    init(date:Date, difficulty: Int, score: Int){
+        self.date = date
+        self.difficulty = difficulty
+        self.score = score
+    }
+    
+}
+
+struct ETResults: Codable{
+    let etResults: [ETResult]
+}
